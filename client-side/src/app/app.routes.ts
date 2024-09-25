c7d26797e518d651b7cf931022d36f6acba8f43b
noaRechnitzer <noa67661@gmail.com>
2024-09-26 00:08:45 +0300

    add routes

--- client-side/src/app/app.routes.ts
+++ client-side/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { LoginFormComponent } from './pages/login-form/login-form.component';
 import { RecipeDetailsComponent } from './pages/recipe-details/recipe-details.component';
 import { authGuard } from './shared/guards/auth.guard';
-
+  
 export const routes: Routes = [
+    { path: '', loadComponent: () => import('./pages/login-form/login-form.component').then(f => f.LoginFormComponent)   },
+    { path: '**', loadComponent: () => import('./pages/login-form/login-form.component').then(f => f.LoginFormComponent)  },
     { path: 'login', loadComponent: () => import('./pages/login-form/login-form.component').then(f => f.LoginFormComponent) },
     { path: 'personal-area', loadComponent: () => import('./pages/personal-area/personal-area.component').then(f => f.PersonalAreaComponent) , canActivate: [authGuard]},
     // { path: 'recipes', loadComponent: () => import('./pages/recipes-list/recipes-list.component').then(f => f.RecipesListComponent) , children: [
